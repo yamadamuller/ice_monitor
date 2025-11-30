8ac7c6d443d62427245bd381945c0072031601e1
yamadamuller <mateusyamada@alunos.utfpr.edu.br>
2025-11-30 17:49:05 -0300

    [root] Working version of the sweep monitor in Chart.js

--- web_monitor.py
+++ web_monitor.py
@@ -13,8 +13,6 @@ import time
 import os
 import datetime
 import numpy as np
-import pandas as pd
-import csv
 from dotenv import load_dotenv
 load_dotenv() #load environmental variables
 
@@ -44,34 +42,6 @@ n_modes = configs["visualization"]["n_modes"] #number of acquisition pairs
 n_mean = configs["visualization"]["n_samples"] #every n samples compute the mean
 last_n_hours = configs["visualization"]["time_window"] #threshold to mask timestamps on plot
 
-def writter_main(rel_acq_file:str):
-    print(f'[WritterMain] Launched writting process')
-    columns = ["timestamp", "mode", "1000 Z", "1000 TD", "10000 Z", "10000 TD", "100000 Z", "100000 TD", "1000000 Z", "1000000 TD"]
-
-    #synthetic data
-    synth_filename = "./testICE_24_11_25/c_test.csv"
-    data = pd.read_csv(synth_filename).to_numpy()
-    data_idx = 0
-    datestamp = datetime.datetime.now()  # timestamp of the acquisition
-    os.makedirs(os.path.dirname(rel_acq_file), exist_ok=True) if os.path.dirname(rel_acq_file) else None #create file if it doesn't exist
-
-    with open(rel_acq_file, 'a', buffering=1) as file:
-        writer = csv.writer(file)
-        writer.writerow(columns)
-        while True:
-            row = (
-                data[data_idx, 0], data[data_idx, 1], data[data_idx, 2], data[data_idx, 3],
-                data[data_idx, 4], data[data_idx, 5], data[data_idx, 6], data[data_idx, 7],
-                data[data_idx, 8], data[data_idx, 9]
-            )
-
-            writer.writerow(row)
-            file.flush()
-            os.fsync(file.fileno())
-            data_idx += 1  # increase the times
-            if data_idx == len(data):
-                break
-
 #constantly read from the buffer CSV and push to the sample buffer in memory
 def producer_main(csv_path:str, buffer:multiprocessing.Queue, buffer_event:multiprocessing.synchronize.Event):
     print(f'[ProducerMain] Launched producer process')
@@ -93,64 +63,90 @@ def producer_main(csv_path:str, buffer:multiprocessing.Queue, buffer_event:multi
             line = line.strip().replace(' ', '') #strip spaces in the string
             arr = np.array(line.split(',')) #split on commas (reading value)
             if not buffer.full():
-                buffer.put(arr) #push to the buffer
+                if not "timestamp" in arr:
+                    buffer.put(arr) #push to the buffer
 
         file.close() #close file to exit
 
 #constantly consume samples from the buffer in memory
 def consumer_main(plot_buffer:multiprocessing.Queue, buffer:multiprocessing.Queue, buffer_event:multiprocessing.synchronize.Event):
     print(f'[ConsumerMain] Launched consumer process')
-    batch = {}  #dictionary to handle acquisition modes
+
+    mode_order = [
+        "d:10-1","d:9-10","d:8-9","d:7-8","d:6-7",
+        "d:5-6","d:4-5","d:3-4","d:2-3","d:1-2"
+    ]
+
+    #create the batch structure for all electrode modes
+    batch = {
+        m: {
+            "timestamp": [],
+            "cap": None,
+            "res": None,
+            "count": 0,
+            "ready": False #ensure all 10 modes have been processed in sequence (avoids sending empty modes to the api)
+        }
+        for m in mode_order
+    }
+
     while True:
         if buffer_event.is_set():
             break
 
-        if not buffer.empty():
-            try:
-                sample = buffer.get() #acquire a sample from the buffer
-                timestamp = int(sample[0]) #unix timestamp
-                electrode_mode = str(sample[1].item()) #electrode mode
-                cap_readings = sample[idx_caps].astype(float) #capacitance reading
-                res_readings = sample[idx_res].astype(float) #resistance reading
-
-                #handle if some samples have already been acquired
-                if electrode_mode in list(batch.keys()):
-                    if len(np.atleast_2d(batch[electrode_mode]["cap"])) < n_mean:
-                        batch[electrode_mode]["timestamp"].append(timestamp) #append the timestamp to the mode dictionary
-                        batch[electrode_mode]["cap"] = np.vstack([batch[electrode_mode]["cap"], cap_readings]) #append the capacitance to the mode dictionary
-                        batch[electrode_mode]["res"] = np.vstack([batch[electrode_mode]["res"], res_readings]) #append the resistance to the mode dictionary
-                    else:
-                        if n_mean > 1:
-                            plot_buffer.put(
-                                    {
-                                        "mode": electrode_mode, #which mode
-                                        "avg_timestamp": np.mean(batch[electrode_mode]["timestamp"], axis=0), #mean of the timestamps
-                                        "avg_cap": np.mean(batch[electrode_mode]["cap"], axis=0), #mean of the capacitance
-                                        "avg_res": np.mean(batch[electrode_mode]["res"], axis=0) #mean of the resistance
-                                    }
-                            ) #if n_mean samples have been appended -> push to the plot buffer
-
-                        else:
-                            plot_buffer.put(
-                                {
-                                    "mode": electrode_mode,  # which mode
-                                    "avg_timestamp": np.mean(batch[electrode_mode]["timestamp"], axis=0),
-                                    "avg_cap": batch[electrode_mode]["cap"],  # mean of the capacitance
-                                    "avg_res": batch[electrode_mode]["res"]  # mean of the resistance
-                                }
-                            ) #if n_mean samples have been appended -> push to the plot buffer
-
-                        del batch[electrode_mode] #reset the batch after mean
-                else:
-                    batch[electrode_mode] = {
-                            "timestamp": [timestamp],
-                            "cap": cap_readings,
-                            "res": res_readings,
-                    } #if the mode hasn't been registered yet
-            except:
-                continue #if something fails during the registering
-        else:
+        if buffer.empty():
             time.sleep(1e-3)
+            continue
+
+        try:
+            sample = buffer.get() #acquire a sample form the buffer
+            timestamp = int(sample[0]) #convert unix timestamp to integer
+            mode = str(sample[1].item()) #which electrode mode is used
+            cap_readings = sample[idx_caps].astype(float) #capacitance reading
+            res_readings = sample[idx_res].astype(float) #resistance reading
+
+            #handle mode-specific batch
+            b = batch[mode] #update the batch for the input mode
+            b["timestamp"].append(timestamp) #append the timestamp to the mode dictionary
+
+            #in case first capacitance reading
+            if b["cap"] is None:
+                b["cap"] = np.atleast_2d(cap_readings) #ensure dimensions
+                b["res"] = np.atleast_2d(res_readings) #ensure dimensions
+            else:
+                b["cap"] = np.vstack([b["cap"], cap_readings]) #append to the array and keep frequency-dependent format
+                b["res"] = np.vstack([b["res"], res_readings]) #append to the array and keep frequency-dependent format
+
+            b["count"] += 1 #increase counter
+
+            #set ready when all the samples have been processed
+            if b["count"] == n_mean:
+                b["ready"] = True
+
+            #if "n_mean" samples have been appended -> push all batches to the plot buffer
+            if all(batch[m]["ready"] for m in mode_order):
+                for m in mode_order:
+                    bm = batch[m]
+                    plot_buffer.put({
+                        "mode": m,
+                        "avg_timestamp": np.mean(bm["timestamp"]), #mean of the "n_mean" timestamps
+                        "avg_cap": np.mean(bm["cap"], axis=0), #mean of the "n_mean" capacitance
+                        "avg_res": np.mean(bm["res"], axis=0) #mean of the "n_mean" resistance
+                    })
+
+                #reset batch for next mode sweep
+                batch = {
+                    m: {
+                        "timestamp": [],
+                        "cap": None,
+                        "res": None,
+                        "count": 0,
+                        "ready": False
+                    }
+                    for m in mode_order
+                }
+
+        except Exception as e:
+            print("[ConsumerMain] Exception:", e)
             continue
 
 #global variable for server launch
@@ -171,7 +167,9 @@ def index():
                            freqs=freqs.tolist())
 
 def frame_processing(plot_buffer:multiprocessing.Queue, buffer_event:multiprocessing.synchronize.Event):
+    global last_proc_image
     print(f'[FrameProcessing] Launched API thread!')
+
     #plotting helpers
     capacitance_full = []
     resistance_full = []
@@ -222,10 +220,10 @@ def frame_processing(plot_buffer:multiprocessing.Queue, buffer_event:multiproces
                     for sweep_freq in freqs:
                         freq_indx = freqs ==sweep_freq
                         last_proc_image[str(int(sweep_freq))] = {
-                            "cap_mtx": cap_matrix[:,:,freq_indx].tolist(),
-                            "res_mtx": res_matrix[:,:,freq_indx].tolist(),
+                            "cap_mtx": autoconstrast(cap_matrix[:,:,freq_indx]).tolist(),
+                            "res_mtx": autoconstrast(res_matrix[:,:,freq_indx]).tolist(),
                             "timestamp": human_timestamp.tolist()
-                        } #update frequency specific image api
+                        }
 
                     img_counter = 0 #reset the counter
                     cap = np.zeros((n_modes, n_freqs)) #reset the batch-specific capacitance matrix
@@ -233,6 +231,7 @@ def frame_processing(plot_buffer:multiprocessing.Queue, buffer_event:multiproces
             except:
                 continue  #in case something goes wrong processing a frame/batch, continue to not freeze the image
         else:
+            time.sleep(1e-3)
             continue
 
 @app.route('/monitor_start')
@@ -252,17 +251,6 @@ def start_data_acquisition():
 def stop_data_acquisition():
     global visu_event, buffer, plots, last_proc_image
     visu_event.set() #set the visualization event to end
-
-    #if exited the recording loop, terminate all processes
-    if write_process and write_process.is_alive():
-        print(f'[StartMonitoring] Terminating producer process with PID = {write_process.pid}', flush=True)
-        write_process.terminate()
-        write_process.join(timeout=1) #timeout in case the process freezes on shutdown
-        if not write_process.is_alive():
-            print(f'[StartMonitoring] Process {write_process.pid} terminated!', flush=True)
-        else:
-            write_process.kill()
-            print(f'[StartMonitoring] Process {write_process.pid} killed!', flush=True)
 
     if ipc_runner.producer_process and ipc_runner.producer_process.is_alive():
         print(f'[StartMonitoring] Terminating producer process with PID = {ipc_runner.producer_process.pid}', flush=True)
@@ -308,19 +296,14 @@ def monitor_status():
 @app.route("/api/<sweeped_freq>Hz")
 @cross_origin()
 def frequency_data_api(sweeped_freq):
-    try:
-        if last_proc_image[sweeped_freq]:
-            return jsonify(last_proc_image[sweeped_freq])
-    except:
-        pass
+    if last_proc_image[sweeped_freq]:
+        return jsonify(last_proc_image[sweeped_freq])
 
 if __name__ == '__main__':
     # buffers in memory
     buffer = multiprocessing.Queue()
     plots = multiprocessing.Queue()
     visu_event = multiprocessing.Manager().Event()  # event to signal both threads to stop
-    write_process = Process(target=writter_main, args=(csv_file,))
-    write_process.start()
 
     #CSV file verifications
     while True:
